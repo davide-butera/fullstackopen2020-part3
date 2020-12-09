564210eeab2258aca4c991fb771cb3a08a4291ba
davide-butera <davidebtr8@example.com>
2020-12-09 21:04:21 +0100

    update

--- index.js
+++ index.js
@@ -30,10 +30,7 @@ let persons = [
 ]
 app.use(express.json())
 app.use(express.static('build'))
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
+
 
 morgan.token('body', function (req, res) {  if(req.method === "POST") return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
